e0bff6edd76445a260c08ae76d745553a3679f24
BinoManjesh <binomanjesh@gmail.com>
2019-06-20 19:20:55 +0530

    Renamed and changed DecimalConverter

--- NumberDecimalConverter.java
+++ NumberDecimalConverter.java
@@ -3,19 +3,10 @@ import java.util.Scanner;
 /**
  * Converts a number in any base to base 10
  */
-class DecimalConverter {
-
-    int base;
-    String num;
-    double decimal;
-
-    DecimalConverter (int base, String num) {
-        this.base = base;
-        this.num = num;
-        decimal = 0.0;
-    }
-
-    void calculate () {
+class NumberDecimalConverter {
+	
+    double getVal (int base, String num) {
+		double decimal = 0.0;
         int j = num.indexOf('.');
         int length = num.length();
         if (j == -1) {
@@ -35,24 +26,18 @@ class DecimalConverter {
             }
             j--;
         }
-    }
-
-    void print () {
-        System.out.println("Decimal: " + decimal);
+		return decimal;
     }
 
     public static void main () {
         Scanner sc = new Scanner(System.in);
+		NumberDecimalConverter dc = new NumberDecimalConverter();
         do {
             System.out.print("Enter the base: ");
             int base = sc.nextInt();
             System.out.print("Enter the number: ");
             String num = sc.next();
-
-            DecimalConverter dc = new DecimalConverter(base, num);
-            dc.calculate();
-            dc.print();
-
+            System.out.println(num + " in decimal is: " + dc.getVal(base, num));
             System.out.print("Enter \"y\" to continue: ");
         } while (sc.next().equals("y"));
     }
