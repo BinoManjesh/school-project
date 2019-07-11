f38b14bdaf6eae10a345e0479b7d0ebc90fbb101
BinoManjesh <binomanjesh@gmail.com>
2019-07-11 17:27:38 +0530

    Changed the motion to be based on rps not angular velocity

--- infinite_acceleration/Ball.java
+++ infinite_acceleration/Ball.java
@@ -4,25 +4,25 @@ import java.awt.*;
 
 class Ball {
 
-    private static final float INIT_ANGULAR_VELOCITY = 0;
-    private static final float ANGULAR_ACCELERATION = (float) (Math.PI / 2);
+    private static final float INIT_RPS = 20;
+    private static final float RPS_INCREASE_RATE = 0.25f;
     private static final int MOVEMENT_RADIUS = 100;
     private static final int BALL_SIZE = 50;
     private static final float COLOR_CHANGE_RATE = 0.01f;
 
     private float angle;
-    private float angularVelocity;
+    private float rps;
     private float red;
 
     Ball() {
         angle = 0;
-        angularVelocity = INIT_ANGULAR_VELOCITY;
-        red = 0;
+        rps = INIT_RPS;
+        red = COLOR_CHANGE_RATE * INIT_RPS / RPS_INCREASE_RATE;
     }
 
     void update(float delta) {
-        angularVelocity += ANGULAR_ACCELERATION * delta;
-        angle += angularVelocity * delta;
+        rps += RPS_INCREASE_RATE * delta;
+        angle += rps * Math.PI * 2 * delta;
         red += COLOR_CHANGE_RATE * delta;
         if (red > 1) {
             red = 1;
@@ -35,7 +35,6 @@ class Ball {
         g.setColor(new Color(red, 0, 1 - red, 1));
         g.fillOval(x, y, BALL_SIZE, BALL_SIZE);
         g.setColor(Color.BLACK);
-        g.drawString("angular velocity(radians): " + angularVelocity, 0, 20);
-        g.drawString("angular velocity(degrees): " + angularVelocity * 180 / Math.PI, 0, 30);
+        g.drawString("rps: " + rps, 0, 20);
     }
 }
