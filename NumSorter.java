c22de0317f677acfa9b3c1a63ad1ea48e1cf952e
BinoManjesh <binomanjesh@gmail.com>
2019-07-11 17:27:04 +0530

    Finished numSorter

--- NumSorter.java
+++ NumSorter.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class NumSorter {
 
-    public static void main() {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int n = sc.nextInt();
@@ -11,22 +11,31 @@ class NumSorter {
         System.out.println(s.sort(n));
     }
 
-    int sort(int n) {
+    private int sort(int n) {
         int sorted = 0;
         while (n > 0) {
-            int digit = n % 10;
-            int c = sorted;
-            int power = 1;
-            while (c > 0) {
-                if (digit >= c % 10) {
-                    sorted = (c % 10 * power + digit) * power + sorted - (c % 10 * power);
-                    break;
-                }
-                power *= 10;
-                c /= 10;
-            }
+            int dig1 = n % 10;
+            int pos = getPos(dig1, sorted);
+            sorted = insert(dig1, pos, sorted);
             n /= 10;
         }
         return sorted;
+    }
+
+    private int getPos(int n, int target) {
+        int i = 0;
+        do {
+            int dig = target % 10;
+            if (n > dig) {
+                return i;
+            }
+            i++;
+            target /= 10;
+        } while (target > 0);
+        return i;
+    }
+
+    private int insert(int n, int i, int target) {
+        return (target / (int) Math.pow(10, i)) * (int) Math.pow(10, i + 1) + n * (int) Math.pow(10, i) + target % (int) Math.pow(10, i);
     }
 }
