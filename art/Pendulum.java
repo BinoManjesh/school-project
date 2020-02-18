ca68619fa2cb86abd761571009af51792d7535b4
BinoManjesh <binomanjesh@gmail.com>
2020-02-18 22:17:47 +0530

    Made acceleration due to gravity changeable

--- art/Pendulum.java
+++ art/Pendulum.java
@@ -1,19 +1,16 @@
 package art;
 
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
+import java.awt.event.*;
 
-class Pendulum extends Animation implements MouseListener, MouseMotionListener {
-
-    private static final double G = 10;
+class Pendulum extends Animation implements MouseListener, MouseMotionListener, KeyListener {
 
     private static final double SIZE_FRACTION = 0.1;
 
     private static final Color PRECISE = new Color(0, 0, 1, 0.5f);
     private static final Color APPROXIMATION = new Color(1, 0, 0, 0.5f);
 
+    private double g = 10;
     private Bob bob1;
     private Bob bob2;
     private boolean dragging = false;
@@ -25,22 +22,24 @@ class Pendulum extends Animation implements MouseListener, MouseMotionListener {
         bob2.theta = Math.PI / 2;
         frame.addMouseListener(this);
         frame.addMouseMotionListener(this);
+        frame.addKeyListener(this);
     }
 
     public static void main(String[] args) {
+        System.out.println("Press Q to increase gravity and E to decrease gravity");
+        System.out.println("Drag the mouse to reset the bobs");
         new Pendulum().start();
     }
 
     @Override
     void update(double dt) {
         if (!dragging) {
-            bob1.omega -= G * Math.sin(bob1.theta) * dt;
+            bob1.omega -= g * Math.sin(bob1.theta) * dt;
             bob1.theta += bob1.omega * dt;
 
-            bob2.omega -= G * bob2.theta * dt;
+            bob2.omega -= g * bob2.theta * dt;
             bob2.theta += bob2.omega * dt;
         }
-
     }
 
     @Override
@@ -79,12 +78,25 @@ class Pendulum extends Animation implements MouseListener, MouseMotionListener {
 
     @Override
     public void mouseDragged(MouseEvent e) {
-        System.out.println("jeff");
         bob1.omega = 0;
         bob2.omega = 0;
         double theta = Math.atan((e.getX() - Bob.centerX) / (e.getY() - Bob.centerY));
         bob1.theta = theta;
         bob2.theta = theta;
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int ch = e.getKeyChar();
+        System.out.println("g: " + g);
+        switch (ch) {
+            case 'q':
+                g /= 0.9;
+                break;
+            case 'e':
+                g *= 0.9;
+                break;
+        }
     }
 
     @Override
@@ -98,4 +110,10 @@ class Pendulum extends Animation implements MouseListener, MouseMotionListener {
 
     @Override
     public void mouseMoved(MouseEvent e) {}
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    @Override
+    public void keyReleased(KeyEvent e) {}
 }
