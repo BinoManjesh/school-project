995884402b9cceaeeaa574334af0198bc04aed8c
BinoManjesh <binomanjesh@gmail.com>
2020-03-27 14:47:41 +0530

    No commit message

--- GravitySimulatorLauncher.java
+++ GravitySimulatorLauncher.java
@@ -2,6 +2,21 @@ import com.bino.gravitation.GravitationGame;
 
 class GravitySimulatorLauncher {
 
+    /*
+    Controls:
+    space bar: to pause or resume the simulation (it is paused at first)
+    + / - : Make time go faster or slower
+    scroll: to zoom in or out
+    drag the mouse to move around the universe
+    click anywhere to create a planet
+    click on a planet to select it
+    drag a planet with left click to move it
+    drag a planet with right click to impart velocity (shown by the blue line)
+    a / d : increase or decrease the size of the selected(red) planet
+    backspace : delete the selected planet
+    enter : delete all planets
+     */
+
     public static void main(String[] args) {
         Launcher.launch(new GravitationGame());
     }
