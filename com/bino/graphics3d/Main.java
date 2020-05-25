0f3646d08775f3ddd14bbb6819f05dc1113a1a51
BinoManjesh <binomanjesh@gmail.com>
2020-05-25 17:22:10 +0530

    no u

--- com/bino/graphics3d/Main.java
+++ com/bino/graphics3d/Main.java
@@ -7,78 +7,102 @@ import java.util.Comparator;
 
 class Main {
 
-    private static final int SCREEN_WIDTH = 1000, SCREEN_HEIGHT = 500;
+	private static final int SCREEN_WIDTH = 1000, SCREEN_HEIGHT = 500;
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Mesh original = new Mesh("C:\\Users\\HOME\\Desktop\\something\\school-project\\com\\bino\\graphics3d\\cube" +
-                ".obj");
-        Mesh cube = original.copy();
+		Mesh original = new Mesh("cube.obj");
+		Mesh cube = original.copy();
 
-        JComponent c = new JComponent() {
-            @Override
-            public void paintComponent(Graphics g) {
-                g.setColor(Color.GRAY);
-                g.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-                g.setColor(Color.WHITE);
-                for (Triangle triangle : cube.triangles) {
-                    Vector2[] projected = new Vector2[3];
-                    double k = SCREEN_WIDTH / 2.0;
-                    for (int i = 0; i < 3; ++i) {
-                        projected[i] = project(k, triangle.points[i]);
-                        projected[i].x = SCREEN_WIDTH / 2.0 + projected[i].x;
-                        projected[i].y = SCREEN_HEIGHT / 2.0 - projected[i].y;
-                    }
-                    g.drawLine((int) projected[0].x, (int) projected[0].y, (int) projected[1].x, (int) projected[1].y);
-                    g.drawLine((int) projected[1].x, (int) projected[1].y, (int) projected[2].x, (int) projected[2].y);
-                    g.drawLine((int) projected[2].x, (int) projected[2].y, (int) projected[0].x, (int) projected[0].y);
-                }
-            }
-        };
-        c.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+		for (int i = 0; i < cube.triangles.size(); ++i) {
+			for (int j = 0; j < 3; ++j) {
+				cube.triangles.get(i).points[j] =
+				original.triangles.get(i).points[j].add(0, 0, -10);
+			}
+		}
 
-        SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("3d graphics");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setResizable(false);
+		JComponent c = new JComponent() {
+			@Override
+			public void paintComponent(Graphics g) {
+				g.setColor(Color.GRAY);
+				g.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+				g.setColor(Color.BLACK);
+				for (Triangle triangle : cube.triangles) {
+					Vector2[] projected = new Vector2[3];
+					double k = SCREEN_WIDTH / 2.0;
+					for (int i = 0; i < 3; ++i) {
+						projected[i] = project(k, triangle.points[i]);
+						projected[i].x = SCREEN_WIDTH / 2.0 + projected[i].x;
+						projected[i].y = SCREEN_HEIGHT / 2.0 - projected[i].y;
+					}
+					fillTriangle(g, projected);
+				}
+                // fillTriangle(g, new Vector2[] {new Vector2(700, 400), new Vector2(100, 400), new Vector2(0, 0)});
+			}
+		};
+		c.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
 
-            frame.add(c);
-            frame.pack();
-            frame.setVisible(true);
-        });
+		SwingUtilities.invokeLater(() -> {
+			JFrame frame = new JFrame("3d graphics");
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setResizable(false);
 
-        long prevTime = System.nanoTime();
-        double t1 = 0, t2 = 0;
-        while (true) {
-            long thisTime = System.nanoTime();
-            double dt = (thisTime - prevTime) * 1e-9;
-            prevTime = thisTime;
+			frame.add(c);
+			frame.pack();
+			frame.setVisible(true);
+		});
 
-            t1 += Math.PI * dt;
-            t2 += Math.PI * dt;
+        // long prevTime = System.nanoTime();
+        // double t1 = 0, t2 = 0;
+        // while (true) {
+        //     long thisTime = System.nanoTime();
+        //     double dt = (thisTime - prevTime) * 1e-9;
+        //     prevTime = thisTime;
 
-            for (int i = 0; i < cube.triangles.size(); ++i) {
-                for (int j = 0; j < 3; ++j) {
-                    cube.triangles.get(i).points[j] =
-                            original.triangles.get(i).points[j].rotateX(t1).rotateZ(t2).add(0, 0, -4);
-                }
-            }
+        //     t1 += Math.PI * dt / 2;
+        //     t2 += Math.PI * dt;
 
-            c.repaint();
-        }
-    }
 
-    static void fillTriangle(Vector2 p1, Vector2 p2, Vector2 p3) {
-        Vector2[] p = new Vector2 {p1, p2, p3}
-        Arrays.sort(p, new Comparator<Vector2>() {});
 
-        int y = (int) highest.y;
-    }
+        //     c.repaint();
+        // }
+	}
 
-    static Vector2 project(double k, Vector3 p) {
-        Vector2 projected = new Vector2();
-        projected.x = -k * p.x / p.z;
-        projected.y = -k * p.y / p.z;
-        return projected;
-    }
+	static void fillTriangle(Graphics g, Vector2[] p) {
+		Arrays.sort(p, (a, b) -> (int)(a.y - b.y));
+
+		int y = (int)p[0].y;
+		double m1 = (p[0].x - p[1].x) / (p[0].y - p[1].y);
+		double c1 = p[0].x - m1 * p[0].y;
+		double m2 = (p[0].x - p[2].x) / (p[0].y - p[2].y);
+		double c2 = p[0].x - m1 * p[0].y;
+		while (y + 0.5 <= p[1].y) {
+			double x1 = m1*(y + 0.5) + c1;
+			double x2 = m2*(y + 0.5) + c2;
+			g.drawLine((int)x1, y, (int)x2, y);
+			y++;
+		}
+		m1 = (p[2].x - p[1].x) / (p[2].y - p[1].y);
+		c1 = p[2].x - m1 * p[2].y;
+		while (y + 0.5 <= p[2].y) {
+			double x1 = m1*(y + 0.5) + c1;
+			double x2 = m2*(y + 0.5) + c2;
+			g.drawLine((int)x1, y, (int)x2, y);
+			y++;
+		}
+	}
+
+	static void drawTriangle(Graphics g, Vector2[] p) {
+		for (int i = 0; i < 3; ++i) {
+			int j = (i + 1) % 3;
+			g.drawLine((int)p[i].x, (int)p[i].y, (int)p[j].x, (int)p[j].y);
+		}
+	}
+
+	static Vector2 project(double k, Vector3 p) {
+		Vector2 projected = new Vector2();
+		projected.x = -k * p.x / p.z;
+		projected.y = -k * p.y / p.z;
+		return projected;
+	}
 }
